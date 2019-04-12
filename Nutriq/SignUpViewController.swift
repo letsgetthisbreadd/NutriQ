6f0a18b36bda328121c774dce350e680500f98f5
MikeMcmanus95 <mikemcmanus95@gmail.com>
2019-04-12 18:37:50 -0400

    Added some view controlers and storyboards.

---
+++ Nutriq/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  Nutriq
+//
+//  Created by Michael Mcmanus on 4/12/19.
+//  Copyright © 2019 NutriQ. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Nutriq/SurveyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SurveyViewController.swift
+//  Nutriq
+//
+//  Created by Michael Mcmanus on 4/12/19.
+//  Copyright © 2019 NutriQ. All rights reserved.
+//
+
+import UIKit
+
+class SurveyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
